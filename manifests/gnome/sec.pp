73d2b6e9c87a8ae5a2083045ba8920dd9fa2b6b2
ralph-wright <ralph.wright@onyxpoint.com>
2016-06-06 15:53:40 -0400

    (SIMP-1126) Update type/provider for RHEL7 GUI
    SIMP-778 #comment Research for this is in SIMP-778 ticket.
    SIMP-1126 #close
    
    Change-Id: Ib74d6c5bc4641db2342cb98979065f0fb3a7ebc6

--- manifests/gnome/sec.pp
+++ manifests/gnome/sec.pp
@@ -7,27 +7,31 @@
 # * Trevor Vaughan <tvaughan@onyxpoint.com>
 #
 class windowmanager::gnome::sec {
+  compliance_map()
+
   if ( versioncmp($::operatingsystemmajrelease, '6') > 0 ) {
     # TODO: Screensaver settings here
   } else {
     gconf { 'screensaver_enabled':
-      value   => true,
-      type    => 'bool',
-      schema  => 'mandatory',
-      require => Package['gnome-screensaver']
+      value  => true,
+      type   => 'bool',
+      schema => 'mandatory',
     }
 
     gconf { 'screensaver_timeout':
-      value   => '15',
-      type    => 'int',
-      require => Package['gnome-screensaver']
+      value => '15',
+      type  => 'int',
     }
 
     gconf { 'screensaver_lock':
-      value   => true,
-      type    => 'bool',
-      schema  => 'mandatory',
-      require => Package['gnome-screensaver']
+      value  => true,
+      type   => 'bool',
+      schema => 'mandatory',
     }
+  }
+
+  # If gdm is greater than 3, then we need to use dconf to secure the desktop
+  if ( versioncmp($::gdm_version, '3') >= 0 ) {
+    include 'windowmanager::gnome::dconf'
   }
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
 describe 'windowmanager' do
-
-  it { is_expected.to create_class('windowmanager') }
-  it { is_expected.to contain_class('mozilla::firefox') }
-
+context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts){ facts }
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_class('windowmanager') }
+      end
+    end
+  end
 end
