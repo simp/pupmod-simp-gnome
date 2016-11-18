f8aca3aae401498d56822eeb357943f4f1de4c32
Chris Tessmer <chris.tessmer@onyxpoint.com>
2016-11-18 06:13:49 -0500

    (SIMP-1868) Remove `compliance_markup`
    This patch removes `compliance_markup` and provides general housekeeping
    
    SIMP-1963 #close
    SIMP-1868 #commment updated pupmod-simp-windowmanager
    SIMP-2052 #comment rolled gconf type changes into this ticket to
    expedite SIMP 6 Alpha release
    
    Change-Id: I497912c21282f973fcb4a509edd11169ccdde9d3

--- spec/classes/gnome/sec_spec.rb
+++ spec/classes/gnome/sec_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 describe 'windowmanager::gnome::sec' do
 context 'supported operating systems' do
-    on_supported_os.each do |os, facts|
+    on_supported_os.each do |os, os_facts|
       context "on #{os}" do
-        let(:facts){ facts }
+        let(:facts) { os_facts.merge( { :gdm_version => '3.20.1' } ) }
         it { is_expected.to compile.with_all_deps }
-        if facts[:operatingsystemmajrelease].to_s <= '6'
+        if os_facts[:operatingsystemmajrelease].to_s <= '6'
           it { is_expected.to create_class('windowmanager::gnome::sec') }
           it { is_expected.to contain_gconf('screensaver_enabled') }
           it { is_expected.to contain_gconf('screensaver_timeout') }
           it { is_expected.to contain_gconf('screensaver_lock') }
         end
         context 'with gdm version < 3' do
-          let(:facts) { facts.merge( { :gdm_version => '2.0' } ) }
+          let(:facts) { os_facts.merge( { :gdm_version => '2.0' } ) }
           it { is_expected.to_not contain_class('windowmanager::gnome::dconf') }
         end
         context 'with gdm version >= 3' do
-          let(:facts) { facts.merge( { :gdm_version => '3.0' } ) }
+          let(:facts) { os_facts.merge( { :gdm_version => '3.0' } ) }
           it { is_expected.to contain_class('windowmanager::gnome::dconf') }
         end
       end
