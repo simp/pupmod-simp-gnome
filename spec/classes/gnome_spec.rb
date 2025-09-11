510575130c482495f49dbbfeab3346bac5d1e832
Steven Pritchard <steve@sicura.us>
2025-09-11 16:47:15 -0500

    Clean up for rubocop (#92)
    * Clean up for rubocop
    * Bump simp-rake-helpers and simp-beaker-helpers dependencies
    
    ---------
    
    Co-authored-by: Chris Tessmer <chris.tessmer@onyxpoint.com>

--- spec/classes/gnome_spec.rb
+++ spec/classes/gnome_spec.rb
@@ -10,7 +10,7 @@ packages_common = [
   'gnome-user-docs',
   'nautilus',
   'orca',
-  'yelp'
+  'yelp',
 ]
 
 packages_el8 = [
@@ -36,103 +36,116 @@ describe 'gnome' do
         it { is_expected.to create_class('gnome') }
 
         # gnome::install
+        # rubocop:disable RSpec/RepeatedExample
         packages_common.each do |pkg|
-          it { is_expected.to contain_package(pkg).with_ensure(/\A(present|installed)\Z/) }
+          it { is_expected.to contain_package(pkg).with_ensure(%r{\A(present|installed)\Z}) }
         end
         if os_facts[:os][:release][:major] == '7'
           packages_el7.each do |pkg|
-            it { is_expected.to contain_package(pkg).with_ensure(/\A(present|installed)\Z/) }
+            it { is_expected.to contain_package(pkg).with_ensure(%r{\A(present|installed)\Z}) }
           end
         end
         if os_facts[:os][:release][:major] == '8'
           packages_el8.each do |pkg|
-            it { is_expected.to contain_package(pkg).with_ensure(/\A(present|installed)\Z/) }
+            it { is_expected.to contain_package(pkg).with_ensure(%r{\A(present|installed)\Z}) }
           end
         end
         if os_facts[:os][:release][:full] == '7.4'
           packages_el7_4.each do |pkg|
-            it { is_expected.to contain_package(pkg).with_ensure(/\A(present|installed)\Z/) }
+            it { is_expected.to contain_package(pkg).with_ensure(%r{\A(present|installed)\Z}) }
           end
         end
+        # rubocop:enable RSpec/RepeatedExample
 
         # gnome::config
         it { is_expected.to create_polkit__authorization__basic_policy('Allow anyone to shutdown system') }
         it { is_expected.to create_polkit__authorization__basic_policy('Allow anyone to restart system') }
 
         # data structure driven
-        it { is_expected.to create_dconf__settings('GNOME dconf settings: simp_gnome').with({
-          :ensure        => 'present',
-          :profile       => 'simp_gnome',
-          :settings_hash => {
-            'org/gnome/desktop/media-handling' => {
-              'automount'               => { 'value' => false },
-              'automount-open'          => { 'value' => false },
-              'autorun-never'           => { 'value' => true },
+        it {
+          is_expected.to create_dconf__settings('GNOME dconf settings: simp_gnome').with(
+            ensure: 'present',
+            profile: 'simp_gnome',
+            settings_hash: {
+              'org/gnome/desktop/media-handling' => {
+                'automount' => { 'value' => false },
+                'automount-open' => { 'value' => false },
+                'autorun-never' => { 'value' => true },
+              },
+              'org/gnome/settings-daemon/plugins/media-keys' => {
+                'logout' => { 'value' => "''" },
+              },
+              'org/gnome/settings-daemon/plugins/power' => {
+                'active' => { 'value' => false },
+              },
+              'org/gnome/desktop/session' => {
+                'idle-delay' => { 'value' => 'uint32 900' },
+              },
+              'org/gnome/desktop/lockdown' => {
+                'disable-lock-screen' => { 'value' => false },
+                'disable-show-password' => { 'value' => true },
+              },
+              'org/gnome/desktop/screensaver' => {
+                'idle-activation-enabled' => { 'value' => true },
+                'lock-enabled' => { 'value' => true },
+                'lock-delay' => { 'value' => 'uint32 0' },
+              },
             },
-            'org/gnome/settings-daemon/plugins/media-keys' => {
-              'logout'                  => { 'value' => "''" },
-            },
-            'org/gnome/settings-daemon/plugins/power' => {
-              'active'                  => { 'value' => false },
-            },
-            'org/gnome/desktop/session' => {
-              'idle-delay'              => { 'value' => 'uint32 900' },
-            },
-            'org/gnome/desktop/lockdown' => {
-              'disable-lock-screen'              => { 'value' => false },
-              'disable-show-password'            => { 'value' => true }
-            },
-            'org/gnome/desktop/screensaver' => {
-              'idle-activation-enabled' => { 'value' => true },
-              'lock-enabled'            => { 'value' => true },
-              'lock-delay'              => { 'value' => 'uint32 0' }
-            }
-          }
-        }) }
+          )
+        }
       end
 
       context 'with a more populated packages' do
-        let(:params) {{ :packages => {
-          'good-package' => { 'ensure' => '1.2.3' },
-          'gnome-terminal' => :undef
-        } }}
+        let(:params) do
+          {
+            packages: {
+              'good-package' => { 'ensure' => '1.2.3' },
+              'gnome-terminal' => :undef,
+            },
+          }
+        end
+
         it { is_expected.to create_package('good-package').with_ensure('1.2.3') }
-        it { is_expected.to create_package('gnome-terminal').with_ensure(/\A(present|installed)\Z/) }
+        it { is_expected.to create_package('gnome-terminal').with_ensure(%r{\A(present|installed)\Z}) }
       end
 
       context 'with an overridden dconf_hash' do
-        let(:params) {{
-          'dconf_hash' => {
-            'simp' => {
-              'org/gnome/desktop/background' => {
-                'picture-uri' => { 'value' => '/wallpaper/path' }
-              }
-            }
+        let(:params) do
+          {
+            'dconf_hash' => {
+              'simp' => {
+                'org/gnome/desktop/background' => {
+                  'picture-uri' => { 'value' => '/wallpaper/path' },
+                },
+              },
+            },
           }
-        }}
+        end
+
         it { is_expected.to create_dconf__settings('GNOME dconf settings: simp').with_profile('simp') }
       end
       context 'with multiple profiles in dconf_hash' do
-        let(:params) {{
-          'dconf_hash' => {
-            'simp' => {
-              'org/gnome/desktop/background' => {
-                'picture-uri' => { 'value' => '/wallpaper/path' }
-              }
+        let(:params) do
+          {
+            'dconf_hash' => {
+              'simp' => {
+                'org/gnome/desktop/background' => {
+                  'picture-uri' => { 'value' => '/wallpaper/path' },
+                },
+              },
+              'site' => {
+                'system/proxy/http' => {
+                  'host' => { 'value' => '0.0.0.0' },
+                },
+              },
             },
-            'site' => {
-              'system/proxy/http' => {
-                'host' => { 'value' => '0.0.0.0' }
-              }
-            }
           }
-        }}
+        end
 
         it { is_expected.to create_dconf__settings('GNOME dconf settings: simp').with_profile('simp') }
 
         it { is_expected.to create_dconf__settings('GNOME dconf settings: site').with_profile('site') }
       end
-
     end
   end
 end
